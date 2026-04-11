dfe2e621e73ff2c19455fc9526345ab437fa5113
naiem7788 <nayantazul77@gmail.com>
2026-04-12 00:03:31 +0600

    Push-Pop

---
+++ push-pop.js
@@ -0,0 +1,16 @@
+const ages = [];
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers);
+numbers.push(6);
+numbers.push(7);
+numbers.push(8, 9, 10, 11);
+
+console.log(numbers);
+
+const friends = ['Tom', 'Jerry', 'Mickey', 'Donald'];
+friends.push('Goofy');
+console.log(friends);
+
+friends.pop();
+console.log(friends);
