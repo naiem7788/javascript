22edaf0697ccf09890dc1591e745b5c627abfdaa
naiem7788 <nayantazul77@gmail.com>
2026-04-12 01:13:36 +0600

    Tast-01

---
+++ isArray.js
@@ -0,0 +1,12 @@
+const friends = ["Tom", "Sara", "Bob", "Alice"];
+
+const nums = [];
+
+const food = "P i zz a";
+
+const age = 25;
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(food));
+console.log(Array.isArray(age));
---
+++ task-1.js
@@ -0,0 +1,10 @@
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+console.log(fruits.indexOf("Cherry"));
+
+let abs = "jambura";
+
+abs = "jambura";
+
+fruits[2] = "jambura";
+console.log(fruits);
