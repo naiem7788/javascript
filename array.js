64f33abe830e4328e88f6912c30d043291353830
naiem7788 <nayantazul77@gmail.com>
2026-04-11 23:34:26 +0600

    Index

---
+++ array.js
@@ -0,0 +1,3 @@
+const numbers = [12, 13, 14, 15, 16, 17, 18];
+console.log(numbers);
+console.log(numbers.length);
---
+++ index.js
@@ -0,0 +1,16 @@
+const numbers = [12, 13, 14, 15, 16, 17, 18];
+console.log(numbers);
+console.log(numbers.length);
+
+//get element value by index
+console.log(numbers[3]);
+
+const fourth = numbers[4];
+console.log(fourth);
+
+//set or update element value by index
+let abc = 45;
+abc = 55;
+
+numbers[3] = 55;
+console.log(numbers);
