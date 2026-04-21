953e3c14eb2e9ccbd342335db808ae91a2b7209d
naiem7788 <nayantazul77@gmail.com>
2026-04-21 21:40:44 +0600

    Break

---
+++ loop/break.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if (i >= 5) {
+        break;
+    }
+}
+console.log("Loop ended");
+
+let n = 54;
+while (n > 25) {
+    console.log(n);
+    if (n < 50) {
+        break;
+    }
+    n--;
+}
+console.log("While loop ended");
