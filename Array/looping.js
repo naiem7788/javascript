cae61ab9febded071c5ce9f54e39580fc67649d2
naiem7788 <nayantazul77@gmail.com>
2026-04-22 22:04:05 +0600

    Sort

---
+++ Array/looping.js
@@ -0,0 +1,9 @@
+const friends = ["Elon", "Bill", "Jeff", "Mark"];
+
+for(const friend of friends) {
+    
+}
+for(let i = 0; i < friends.length; i++) {
+    console.log(i);
+    console.log(friends[i]);
+}
---
+++ Array/sort.js
@@ -0,0 +1,8 @@
+const persons = ["Alice", "Bob", "Charlie", "David"];
+const sortedPersons = persons.sort();
+console.log(sortedPersons);
+
+
+const numbers = [5, 2, 9, 1, 5, 6];
+const numbers_asc = numbers.sort();
+console.log(numbers_asc);
