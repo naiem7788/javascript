f221d3bfbbb0617c322df4995db6b861aef6fda6
naiem7788 <nayantazul77@gmail.com>
2026-04-23 11:38:22 +0600

    Perameter

---
+++ function/function.js
@@ -0,0 +1,8 @@
+function biriyani() {
+    console.log('cook the mutton');
+    console.log('add the spices');
+    console.log('add the rice');
+    console.log('cook it for 30 minutes');  
+}
+
+biriyani();
---
+++ function/perameter.js
@@ -0,0 +1,8 @@
+function square(number) {
+    console.log('value of the parameter is ', number);
+    const result = number * number;
+    console.log('the square of given number is ', result);
+
+}
+square(5);
+square(10);
